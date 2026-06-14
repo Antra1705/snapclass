13ca018b3ed3978373fc0665d041de13b4ce5640
Antra1705 <antra.1705@gmail.com>
2026-06-14 18:04:24 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def footer_home():
+    logo_url="https://i.bb.co/YTYGn5qV/logo.png"
+    st.markdown("""
+                <div style="display: flex;items-align: center; justify-content: center; gap:6px; margin-top: 2rem;">
+                    <p style="font-weight:bold; color:white;"> Created with ❤️ by Antra</>
+                </div>
+        
+
+
+
+    """, unsafe_allow_html=True)
